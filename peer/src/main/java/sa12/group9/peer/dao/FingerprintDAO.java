cd9c3a781fea02cb9d07898be91f71c6b9801104
Marcus Pollice <marcus.pollice@gmail.com>
2013-01-19 18:12:29 +0100

    allow db to be written by multiple instances; fix filename of FingerprintDAO

---
+++ peer/src/main/java/sa12/group9/peer/dao/FingerprintDAO.java
@@ -0,0 +1,135 @@
+package sa12.group9.peer.dao;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import sa12.group9.common.beans.SongMetadata;
+import ac.at.tuwien.infosys.swa.audio.Fingerprint;
+
+public class FingerprintDAO implements IFingerprintDAO
+{
+    private DataSource dataSource;
+
+    private String sql_create;
+    private String sql_insert;
+    private String sql_delete;
+    private String sql_findFingerprintByUserAndMetadata;
+    private String sql_getSongForUserAndFingerprint;
+    private String sql_getAllFingerprintsForUser;
+
+    @SuppressWarnings("unused")
+    private void init() throws SQLException
+    {
+        Statement stat = dataSource.getConnection().createStatement();
+        stat.execute(sql_create);
+    }
+    
+    @Override
+    public void persistFingerprint(String user, SongMetadata smd, Fingerprint fp) throws SQLException
+    {
+        PreparedStatement prep = dataSource.getConnection().prepareStatement(sql_insert);
+        prep.setString(1, user);
+        prep.setObject(2, smd);
+        prep.setObject(3, fp);
+        prep.execute();
+    }
+
+    public void deleteFingerprint(String user, SongMetadata smd) throws SQLException
+    {
+        PreparedStatement prep = dataSource.getConnection().prepareStatement(sql_delete);
+        prep.setString(1, user);
+        prep.setObject(2, smd);
+        prep.executeUpdate();
+    }
+
+    public Fingerprint findFingerprintByUserAndMetadata(String user, SongMetadata smd) throws SQLException
+    {
+        Fingerprint fp = null;
+
+        PreparedStatement prep = dataSource.getConnection().prepareStatement(sql_findFingerprintByUserAndMetadata);
+        prep.setString(1, user);
+        prep.setObject(2, smd);
+
+        ResultSet rs = prep.executeQuery();
+        if (rs.next())
+        {
+            fp = (Fingerprint) rs.getObject(3);
+        }
+
+        return fp;
+    }
+
+    public SongMetadata getSongForUserAndFingerprint(String user, Fingerprint fp) throws SQLException
+    {
+        SongMetadata smd = null;
+
+        PreparedStatement prep = dataSource.getConnection().prepareStatement(sql_getSongForUserAndFingerprint);
+        prep.setString(1, user);
+        prep.setObject(2, fp);
+
+        ResultSet rs = prep.executeQuery();
+        if (rs.next())
+        {
+            smd = (SongMetadata) rs.getObject(3);
+        }
+
+        return smd;
+    }
+
+    public List<Fingerprint> getAllFingerprintsForUser(String user) throws SQLException
+    {
+        List<Fingerprint> fingerprints = new ArrayList<Fingerprint>();
+
+        PreparedStatement prep = dataSource.getConnection().prepareStatement(sql_getAllFingerprintsForUser);
+        prep.setString(1, user);
+
+        ResultSet rs = prep.executeQuery();
+        while (rs.next())
+        {
+            fingerprints.add((Fingerprint) rs.getObject(4));
+        }
+
+        return fingerprints;
+    }
+
+    public void setDataSource(DataSource dataSource)
+    {
+        this.dataSource = dataSource;
+    }
+
+    public void setSql_create(String sql_create)
+    {
+        this.sql_create = sql_create;
+    }
+
+    public void setSql_insert(String sql_insert)
+    {
+        this.sql_insert = sql_insert;
+    }
+
+    public void setSql_delete(String sql_delete)
+    {
+        this.sql_delete = sql_delete;
+    }
+
+    public void setSql_findFingerprintByUserAndMetadata(String sql_findFingerprintByUserAndMetadata)
+    {
+        this.sql_findFingerprintByUserAndMetadata = sql_findFingerprintByUserAndMetadata;
+    }
+
+    public void setSql_getSongForUserAndFingerprint(String sql_getSongForUserAndFingerprint)
+    {
+        this.sql_getSongForUserAndFingerprint = sql_getSongForUserAndFingerprint;
+    }
+
+    public void setSql_getAllFingerprintsForUser(String sql_getAllFingerprintsForUser)
+    {
+        this.sql_getAllFingerprintsForUser = sql_getAllFingerprintsForUser;
+    }
+}
---
+++ peer/src/main/java/sa12/group9/peer/dao/IFingerprintDAO.java
@@ -0,0 +1,58 @@
+package sa12.group9.peer.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import sa12.group9.common.beans.SongMetadata;
+import ac.at.tuwien.infosys.swa.audio.Fingerprint;
+
+public interface IFingerprintDAO {
+
+	/**
+     * Persists a Fingerprint in the database
+     * 
+     * @param username
+     * @param the metadata object for the Fingerprint
+     * @param the Fingerprint
+     * @throws SQLException if there occurs a database error
+     */
+	void persistFingerprint(String user, SongMetadata smd, Fingerprint fp) throws SQLException;
+	
+	/**
+     * Deletes a Fingerprint from the database
+     * 
+     * @param username
+     * @param the metadata object for the Fingerprint
+     * @throws SQLException if there occurs a database error
+     */
+	void deleteFingerprint(String user, SongMetadata smd) throws SQLException;
+	
+	/**
+     * Retrieves a Fingerprint associated with username and metadata from the database
+     * 
+     * @param username
+     * @param the metadata object for the Fingerprint
+     * @return the Fingerprint
+     * @throws SQLException if there occurs a database error
+     */
+	Fingerprint findFingerprintByUserAndMetadata(String user, SongMetadata smd) throws SQLException;
+	
+	/**
+     * Retrieves metadata for a user and associated Fingerprint
+     * 
+     * @param username
+     * @param the Fingerprint
+     * @return the metadata object for the Fingerprint
+     * @throws SQLException if there occurs a database error
+     */
+	SongMetadata getSongForUserAndFingerprint(String user, Fingerprint fp) throws SQLException;
+	
+	/**
+     * Retrieves a list of Fingerprints for a user
+     * 
+     * @param username
+     * @return the list of Fingerprints
+     * @throws SQLException if there occurs a database error
+     */
+	List<Fingerprint> getAllFingerprintsForUser(String user) throws SQLException;
+}
