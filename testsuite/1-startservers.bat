c27da04fd7101ab1d6bb94661045adc662ae6bb1
Michael Heil <m.heil@chello.at>
2013-01-20 21:36:20 +0100

    Fixes

--- testsuite/1-startservers.bat
+++ testsuite/1-startservers.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set pathToMongoDB="TODO: set path to MongoDB installation including /bin"
+set pathToMongoDB="C:\Users\Michael\Desktop\mongodb-win32-x86_64-2.2.2\bin"
 
 cd %pathToMongoDB%
 
--- testsuite/2-startpeers.bat
+++ testsuite/2-startpeers.bat
@@ -7,11 +7,19 @@ set peer3="test3 testpw 12300 12301 12302"
 set peer4="test4 testpw 12400 12401 12402"
 set peer5="test5 testpw 12500 12501 12502"
 
-copy "%~dp0swazam.h2.db" "%~dp0..\peer\target\classes\data\"
-
 cd ..
 
-cd peer
+IF EXIST "%~dp0..\peer\target" GOTO STARTPEERS
+
+call mvn install
+
+:STARTPEERS
+
+cd testsuite
+
+copy "%~dp0swazam.h2.db" "%~dp0..\peer\target\classes\data\"
+
+cd ../peer
 
 REM start peers via maven
 start mvn exec:java -Dexec.mainClass="sa12.group9.peer.Launcher" -Dexec.args=%peer1%
