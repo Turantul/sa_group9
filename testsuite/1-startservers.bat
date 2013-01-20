542f801c3710cf4afd8b935f1f732f69bf51cf8b
Michael Heil <m.heil@chello.at>
2013-01-20 22:33:51 +0100

    Script fixes

--- testsuite/1-startservers.bat
+++ testsuite/1-startservers.bat
@@ -8,6 +8,12 @@ start mongod.exe --port 44444 --dbpath "%~dp0swa"
 
 cd %~dp0..
 
+IF EXIST "peer\target" GOTO STARTSERVER
+
+call mvn install
+
+:STARTSERVER
+
 start mvn tomcat:run
 
 cd testsuite
